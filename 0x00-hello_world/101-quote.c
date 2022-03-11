31b0e81dc61488af9e5434d2752ca3a0788bf25c
Adriana <kinahelga@gmail.com>
2022-03-11 20:59:45 +0300

    advanced tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * Main - Entry Point
- * 
- * Return: Always (1) Failure
+ * main - Entry point
+ *
+ * Return: Always 1 (Failure)
  */
 int main(void)
 {
-    fprintf(stderr, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
     return (1);
 }
