1783e354759ef5375b884b203ba4dd903ed225c9
Adriana <kinahelga@gmail.com>
2022-03-14 17:59:39 +0300

    fix 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,27 +7,29 @@
  */
 int main(void)
 {
-    int i;
-    int j;
-    int k;
-    for(i = 0; i < 8; i++)
-    {
-        for(j = 1; j < 9; j++)
-        {
-            for(k = 2; k < 10; k++)
-            {
-                if(i != j && j != k && i != k)
-                {
-                    putchar(i%10 + '0');
-                    putchar(j%10 + '0');
-                    putchar(k%10 + '0');
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-            
-        }
-    }
-    putchar('\n');
-    return(0);
+int i;
+int j;
+int k;
+for (i = 0; i < 8; i++)
+{
+for (j = 1; j < 9; j++)
+{
+for (k = 2; k < 10; k++)
+{
+if (i != j && j != k && i != k && i < j && j < k)
+{
+putchar(i % 10 + '0');
+putchar(j % 10 + '0');
+putchar(k % 10 + '0');
+if (i + j + k != 24)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
