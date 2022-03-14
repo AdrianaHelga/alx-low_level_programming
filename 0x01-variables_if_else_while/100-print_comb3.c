43e7c5d76fef858db9f03bc68eef83b0351ada53
Adriana <kinahelga@gmail.com>
2022-03-14 17:40:27 +0300

    fix 101

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ int main(void)
     {
         for(j = 1; j < 10; j++)
         {
-            if (i != j)
+            if (i != j && i < j)
             {
                 putchar(i%10 +'0');
                 putchar(j%10 + '0');
