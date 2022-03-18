cd0be593ced71c32a3f6d600f5e0bd41f47b7bea
Adriana <kinahelga@gmail.com>
2022-03-18 23:21:26 +0300

    fix 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,30 +1,34 @@
 #include<stdio.h>
 /**
- * main: entry point
+ * main - entry point
  * description: fizzbuzz coding excercise
- * return: 0 (success)
+ * Return: 0 (success)
  */
 
-int main (void)
+int main(void)
 {
 int num;
 for (num = 1; num <= 100; num++)
 {
-if (num % 3 == 0 && num % 5 == 0)
+if (num == 100)
 {
-    printf("FizzBuzz ");
+printf("Buzz");
+}
+else if (num % 3 == 0 && num % 5 == 0)
+{
+printf("FizzBuzz ");
 }
 else if (num % 3 == 0)
 {
-    printf("Fizz ");
+printf("Fizz ");
 }
 else if (num % 5 == 0)
 {
-    printf("Buzz ");
+printf("Buzz ");
 }
 else
 {
-    printf("%d ", num);
+printf("%d ", num);
 }
 }
 putchar('\n');
