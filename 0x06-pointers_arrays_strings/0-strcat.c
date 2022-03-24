3963aa7ae8dd5b12b071da447f63a7f2d37a05ec
Adriana <kinahelga@gmail.com>
2022-03-24 16:43:16 +0300

    strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ *
+ * @dest: pointer to string to be appended to
+ * @src: pointer to string to append
+ *
+ * Return: pointer to first string
+ */
+char *_strcat(char *dest, char *src)
+{
+/*
+* assign the beginning of the first string to start,
+* so as not to loose the adress of the beginning of the first string
+*/
+char *start = dest;
+
+/*loop to move pointer of dest to the last character i.e '\0' */
+while (*dest != '\0')
+{
+dest++;
+}
+
+
+/*
+*appends one character at a time from src to dest
+*this is because after the first while loop
+*dest is pointing to the null character of dest
+*in the first iteration statement
+*/
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+/*include a null character at the end of the dest string*/
+*dest = '\0';
+return (start);
+}
