b48f8b963f0bbf05e12fa1746e1da662bd861a9c
Adriana <kinahelga@gmail.com>
2022-04-22 15:49:29 +0300

    fix 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,14 @@ int a, b, c, d;
 
 for (a = 0; a < 3; a++)
 {
-for (b = 0; b < 4; b++)
+for (b = 0; b < 10; b++)
 {
 for (c = 0; c < 6; c++)
 {
 for (d = 0; d < 10; d++)
+{
+
+if (a <= 1)
 {
 _putchar(a % 10 + '0');
 _putchar(b % 10 + '0');
@@ -25,6 +28,17 @@ _putchar(':');
 _putchar(c % 10 + '0');
 _putchar(d % 10 + '0');
 _putchar('\n');
+}
+
+else if (a == 2 && b < 4)
+{
+_putchar(a % 10 + '0');
+_putchar(b % 10 + '0');
+_putchar(':');
+_putchar(c % 10 + '0');
+_putchar(d % 10 + '0');
+_putchar('\n');
+}
 }
 }
 }
