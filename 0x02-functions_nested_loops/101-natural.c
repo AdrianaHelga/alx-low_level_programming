1933c4cbd2699197bb17eb9c89f536c3d9e98b73
Adriana <kinahelga@gmail.com>
2022-04-22 21:04:45 +0300

    task 101 fix betty

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,33 +1,36 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * @brief 
- * 
+ * main - entry point
+ *
+ * description - program that computes sum of all numbers
+ * divisible by 3 0r 5
+ *
+ * Return: nothing
  */
 
 int main(void)
 {
-	int a;
-	int result = 0;
+int a;
+int result = 0;
 
-	for(a = 0; a < 1024; a++)
-	{
-		if (a % 3 == 0 && a % 5 == 0)
-		{
-			result += a;
-		}
-		else if (a % 3 == 0)
-		{
-			result += a;
-		}
-		else if (a % 5 == 0)
-		{
-			result += a;
-		}
-	}
+for (a = 0; a < 1024; a++)
+{
+if (a % 3 == 0 && a % 5 == 0)
+{
+result += a;
+}
+else if (a % 3 == 0)
+{
+result += a;
+}
+else if (a % 5 == 0)
+{
+result += a;
+}
+}
 
-	printf("%d\n", result);
-	return (0);
-
+printf("%d\n", result);
+return (0);
 
 }
