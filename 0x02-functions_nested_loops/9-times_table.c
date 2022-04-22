3434ac2f5990097080b9258d7c9a38d310c4e84e
Adriana <kinahelga@gmail.com>
2022-04-22 11:24:22 +0300

    fix betty

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,37 +9,37 @@
 /**/
 void times_table(void)
 {
-    int a, b;
-    int result;
+int a, b;
+int result;
 
-    for (a = 0; a < 10; a++)
-    {
-        if(a != 0)
-        {
-        _putchar('\n');
-        }
-        for (b = 0; b < 10; b++)
-        {
-            result = a * b;
-            if(result >= 10)
-            {
-            _putchar(result / 10 + '0');
-            _putchar(result % 10 + '0');
-            }
-            else
-            {
-            if (b != 0)
-            {
-            _putchar(' ');
-            }
-            _putchar(result % 10 + '0');
-            }
-            if(b != 9)
-            {
-            _putchar(',');
-            _putchar(' ');
-            }
-        }
-    }
+for (a = 0; a < 10; a++)
+{
+if (a != 0)
+{
+_putchar('\n');
+}
+for (b = 0; b < 10; b++)
+{
+result = a * b;
+if (result >= 10)
+{
+_putchar(result / 10 + '0');
+_putchar(result % 10 + '0');
+}
+else
+{
+if (b != 0)
+{
+_putchar(' ');
+}
+_putchar(result % 10 + '0');
+}
+if (b != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
 _putchar('\n');
 }
